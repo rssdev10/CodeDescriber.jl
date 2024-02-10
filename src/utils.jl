61322b6f0f1a004b38876f9b5930377b652e31a2
Maxim Lubov <maxlubov@gmail.com>
2024-02-10 14:35:47 +0600

    Add utils functions

--- src/utils.jl
+++ src/utils.jl
@@ -11,3 +11,26 @@ const KNOWN_PROJECT_FILE_NAMES = [
 
 is_project_file(fn::AbstractString) =
     splitpath(fn)[end] in KNOWN_PROJECT_FILE_NAMES
+
+"""
+Converting Dict to a String
+"""
+function dict_to_string(dict::Dict{T, T} where T <: AbstractString, separator = "")
+    parts = String[]
+    for (key, value) in dict
+        push!(parts, "$(key): $(value)")
+    end
+    dict_string = separator == "" ? join(parts, ", \n") : join(parts, ", \n" * separator * "\n")
+    return dict_string
+end
+
+vector_to_string(vec::Vector{T}, separator = "") where T <: AbstractString = 
+    separator == "" ? join(vec, ", \n") : join(vec, ", \n" * separator * "\n") 
+
+function json_to_text(input_file::AbstractString)
+    input_path = joinpath(@__DIR__, "..", "data", input_file)
+    json_data = JSON.parsefile(input_path)
+
+    pretty_json = JSON.json(json_data, 4)
+    return pretty_json
+end
